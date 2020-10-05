94c8525052f52a8c3c7aaa9c4825d3de4955eb26
MagicMarvMan <marvin.kai@gmx.de>
2020-10-05 22:33:12 +0200

    Fix blocklist check

--- src/blocklist.ts
+++ src/blocklist.ts
@@ -4,13 +4,13 @@ import fs from "fs";
 const readFile: Function = promisify(fs.readFile);
 
 export async function checkUrlShortener(url: string) {
-    if((await readFile("blocklists/url_shorteners.txt")).includes(`${url}`)) {
+    if((await readFile("blocklists/url_shorteners.txt")).includes(`${new URL(url).hostname}`)) {
         return false;
     } else return true;
 }
 
 export async function checkPornTop1Million(url: string) {
-    if((await readFile("blocklists/porn_top1m.txt")).includes(`${url}`)) {
+    if((await readFile("blocklists/porn_top1m.txt")).includes(`${new URL(url).hostname}`)) {
         return false;
     } else return true;
 }
