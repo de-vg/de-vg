afca640f4c7068797e8dd609c1898c7a468c7f09
MagicMarvMan <marvin.kai@gmx.de>
2020-10-05 21:32:17 +0200

    Add basic software

---
+++ src/index.ts
@@ -0,0 +1,76 @@
+require('dotenv').config();
+
+// Import dependencies
+import express from "express";
+import mysql2 from "mysql2";
+import bodyParser from "body-parser";
+import i18n from "i18n";
+
+import virustotal from "./virustotal";
+
+
+// Define HTTP port
+const PORT: number = parseInt(process.env.PORT) || 3000;
+
+// Configure i18n
+i18n.configure({
+    locales:['en', 'de'],
+    directory: 'src/locales/',
+    cookie: "locale",
+    queryParameter: "hl",
+    objectNotation: true,
+    defaultLocale: "de"
+});
+
+// Initialize express server object
+const app: express.Application = express();
+
+// Use i18n middleware
+app.use(i18n.init);
+
+app.set("view engine", "pug");
+app.set("views", "src/views/");
+
+// Initialize MySQL
+app.locals.mysql = mysql2.createConnection({
+    host: process.env.MYSQL_SERVER,
+    user: process.env.MYSQL_USERNAME,
+    database: process.env.MYSQL_DATABASE,
+    password: process.env.MYSQL_PASSWORD,
+    // Use SSL client auth
+    /*ssl: {
+        // SSL variables are stored Base64 encoded for better compatibility
+        key: Buffer.from(process.env.MYSQL_KEY, "base64").toString("ascii"),
+        cert: Buffer.from(process.env.MYSQL_CERT, "base64").toString("ascii"),
+        ca: Buffer.from(process.env.MYSQL_CA, "base64").toString("ascii")
+    }*/
+});
+
+
+app.get("/", (req: express.Request, res: express.Response) => {
+    res.render("shorten");
+});
+
+app.post("/", async (req: express.Request, res: express.Response) => {
+    const url: string = req.body.url;
+    if(await virustotal(url)) {
+        res.send("Success!");
+    } else {
+        res.render("shorten", {
+            url: url,
+            error: res.__("ErrorVirustotal")
+        });
+    }
+});
+
+app.get("/:url", (req: express.Request, res: express.Response) => {
+
+});
+
+// Start server
+app.listen(PORT, () => {
+    // Log what port the server is listening on
+    console.log(`Listening on port ${PORT}...`);
+})
+
+
---
+++ src/virustotal.ts
@@ -0,0 +1,14 @@
+require('dotenv').config();
+
+import fetch from "node-fetch";
+
+export default async function checkUrl(url: string) {
+    const response = await fetch(`https://www.virustotal.com/vtapi/v2/url/report?apikey=${process.env.VIRUSTOTAL_KEY}&resource=${url}`);
+    const json = await response.json();
+    if(json.positives >= 1) {
+        // Return false if url is NOT safe
+        return false;
+    } else  {
+        return true;
+    }
+}
